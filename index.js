7a597786f4639abe95e4ad669e9dae7fede94059
MD ASIF KABIR EMON <mdasifkabiremon@MDs-MacBook-Air.local>
2022-11-24 15:27:06 +0800

    jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
@@ -17,6 +18,21 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unathorized access");
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send("forbidden access");
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 app.get("/", (req, res) => {
   res.send("Laptop Bazar API is running");
 });
@@ -24,6 +40,30 @@ app.get("/", (req, res) => {
 const run = async () => {
   try {
     const usersCollection = client.db("LaptopBazer").collection("users");
+
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      if (result) {
+        const token = jwt.token(query, process.env.ACCESS_TOKEN);
+        return res.send({ accessToken: token });
+      }
+      res.status(401).send({ accessToken: "" });
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(user);
+    });
   } finally {
   }
 };
