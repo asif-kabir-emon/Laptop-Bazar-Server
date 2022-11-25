778cc42ed670f4232c9fa82c99af87b86dda5de4
MD ASIF KABIR EMON <mdasifkabiremon@MDs-MacBook-Air.local>
2022-11-25 09:24:41 +0800

    banner added in home page

--- index.js
+++ index.js
@@ -46,7 +46,9 @@ const run = async () => {
       const query = { email: email };
       const result = await usersCollection.findOne(query);
       if (result) {
-        const token = jwt.token(query, process.env.ACCESS_TOKEN);
+        const token = jwt.sign(query, process.env.ACCESS_TOKEN, {
+          expiresIn: "10h",
+        });
         return res.send({ accessToken: token });
       }
       res.status(401).send({ accessToken: "" });
@@ -55,8 +57,20 @@ const run = async () => {
     app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
-      const result = await usersCollection.findOne(query);
+      const result = await usersCollection.find(query).toArray();
       res.send(result);
+    });
+
+    app.post("/userFindCreate", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const result = await usersCollection.find(query).toArray();
+      console.log(result.length);
+      if (result.length < 1) {
+        const newUser = await usersCollection.insertOne(user);
+        return res.send({ result: true });
+      }
+      res.send({ result: true });
     });
 
     app.post("/users", async (req, res) => {
